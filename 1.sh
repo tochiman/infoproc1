d6ded45b10e935989f6d09ff04062d65d526993c
tochiman <yuutotochizawa@gmail.com>
2021-10-19 14:24:45 +0900

    commit

--- 1.sh
+++ 1.sh
@@ -1,16 +1,42 @@
-#! /bin/bash
-#今年度と来年度を変数定義
-This_Year=2021
-Next_Year=2022
+#!/bin/bash
+for yearmonth1 in 2021{04,05,06,07,08,09,10,11,12}
+do
+                case ${yearmonth1} in
+                202105|202107|202108|202110|202112)
+                        for a in `seq -f %02g 1 31`
+                        do
+                                mkdir -p ${yearmonth1}${a}
+                        done
+                ;;
+                *)
+                        for b in `seq -f %02g 1 30`
+                        do
+                                mkdir -p ${yearmonth1}${b}
+                        done
+                ;;
+                esac
+done
+for yearmonth2 in 2022{01,02,03}
+do
+                case ${yearmonth2} in
+                202201)
+                        for a in `seq -f %02g 1 30`
+                        do
+                                mkdir -p ${yearmonth2}${a}
+                        done
+                ;;
+                202202)
+                        for b in `seq -f %02g 1 28`
+                        do
+                                mkdir -p ${yearmonth2}${b}
+                        done
+                ;;
+                202203)
+                        for c in `seq -f %02g 1 31`
+                        do
+                                mkdir -p ${yearmonth2}${c}
+                        done
+                ;;
+                esac
+done
 
-#4月のディレクトリを作成
-April(This_year)
-{
-	This_Month4=04 #今月は何月かの変数定義
-	for i in $(( seq 1 30 ))
-	do
-	mkdir "./$This_year$This_Month4$i"	
-	done
-}
-
-source April
